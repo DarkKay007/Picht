ecc59e575162a161f272507baa73642c56f3b13e
Kai <159480405+DarkKay007@users.noreply.github.com>
2024-06-13 23:44:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
+  plugins: [react()]
+  },
+})
+// vite.config.js
+export default {
+  server: {
     host: '0.0.0.0',
     port: 5173,
   },
-})
+  preview: {
+    port: 8080,
+    host: '0.0.0.0',
+  },
+};
